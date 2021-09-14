855b23b7e969cb556f25a215401929f3c558861d
aravindsasi <aravindsasidharan999@gmail.com>
2021-09-14 12:19:30 +0200

    Modified logic of optimization

--- contribs/application/src/main/java/org/matsim/application/prepare/freight/Optimization.java
+++ contribs/application/src/main/java/org/matsim/application/prepare/freight/Optimization.java
@@ -13,28 +13,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Optimization {
 
-    private static final String OPTIMIZED_TRAFFIC_COUNT_PATH = "D:/Work/docs/" + "optimized-traffic-count.csv";
+    private static final String OPTIMIZED_TRAFFIC_COUNT_PATH = "D:/Work/" + "optimized-traffic-count.csv";
 
-    static String optimization(Network network, Map<String, Map<String, Double>> freightTraffic, Map<String, Id<Link>> zoneIdToLinkIdMap, LeastCostPathCalculator router, Map<Id<Link>, Double> actualCount, Map<Id<Link>, Double> trafficCountReference, double score){
+     public void optimization(Network network, Map<String, Map<String, Double>> freightTraffic, Map<String, Id<Link>> zoneIdToLinkIdMap, LeastCostPathCalculator router, Map<Id<Link>, Double> actualCount, Map<Id<Link>, Double> trafficCountReference, double score){
 
-        Map<Id<Link>, Double> actualCountForReference;
+       // Map<Id<Link>, Double> actualCountForReference;
         Set<Id<Link>> linkIdSet = actualCount.keySet();
-        actualCountForReference = linkIdSet.stream().collect(Collectors.toMap(link -> link, actualCount::get, (a, b) -> b, ConcurrentHashMap::new));
-
+        //actualCountForReference = linkIdSet.stream().collect(Collectors.toMap(link -> link, actualCount::get, (a, b) -> b, ConcurrentHashMap::new));
+        AtomicReference<Double> totalTrips = new AtomicReference<>(0.0);
+        AtomicInteger totalTripsChanged = new AtomicInteger();
         freightTraffic.keySet().forEach(from -> {
-            System.out.println("New source");
+            //System.out.println("New source");
             freightTraffic.get(from).keySet().forEach(to -> {
+                double numOfVehicles = freightTraffic.get(from).get(to);
+                totalTrips.updateAndGet(v -> v + numOfVehicles);
+                if(numOfVehicles > 5){
                 Node fromNode = network.getLinks().get(zoneIdToLinkIdMap.get(from)).getToNode();
                 Node toNode = network.getLinks().get(zoneIdToLinkIdMap.get(to)).getToNode();
-                Set<Id<Link>> links = actualCount.keySet();
+                Set<Id<Link>> linksInActualCount = actualCount.keySet();
                 List<Link> linksInRoute = router.calcLeastCostPath(fromNode, toNode, 0, null, null).links;
-                List<Id<Link>> linkIds = new ArrayList<>();
-                linksInRoute.forEach(link -> linkIds.add(link.getId()));
-                System.out.println("New destination");
+                List<Id<Link>> linkIdsInRoute = new ArrayList<>();
+                linksInRoute.forEach(link -> linkIdsInRoute.add(link.getId()));
+                //System.out.println("New destination");
                 Map<Id<Link>, Double> actualCountDecrease = new ConcurrentHashMap<>();
                 Map<Id<Link>, Double> actualCountIncrease = new ConcurrentHashMap<>();
                 Set<Id<Link>> stations = actualCount.keySet();
@@ -44,8 +49,8 @@ public class Optimization {
                 }
                 int increment = 1;
                 boolean hasStationInRoute = false;
-                for (Id<Link> link : links) {
-                    if (linkIds.contains(link)) {
+                for (Id<Link> link : linksInActualCount) {
+                    if (linkIdsInRoute.contains(link)) {
                         hasStationInRoute = true;
                         double decreaseTrip = actualCountDecrease.get(link) - 1;
                         double increaseTrip = actualCountIncrease.get(link) + 1;
@@ -55,33 +60,44 @@ public class Optimization {
                 }
                 double optimizedScoreAfterDecrease = calculateScore(actualCountDecrease, trafficCountReference);
                 double optimizedScoreAfterIncrease = calculateScore(actualCountIncrease, trafficCountReference);
-                double percentage = 0.0;
+                double percentage;
                 double optimizedPreviousScore = 0.0;
                 if((score > optimizedScoreAfterDecrease || score > optimizedScoreAfterIncrease) && hasStationInRoute){
                     Map<Id<Link>, Double> modifiedActualCount = new ConcurrentHashMap<>();
-                    increment += 1;
-                    if(optimizedScoreAfterDecrease < optimizedScoreAfterIncrease){
+              //      if(optimizedScoreAfterDecrease < optimizedScoreAfterIncrease){
                         Set<Id<Link>> actualCountDecreaseStations = actualCountDecrease.keySet();
                         for(Id<Link> link : actualCountDecreaseStations){
                             modifiedActualCount.put(link, actualCountDecrease.get(link));
                         }
-
+                        double vehicleCount = numOfVehicles;
+                        int changeInTrip;
                         while(increment > 0) {
+                            if(optimizedScoreAfterDecrease < optimizedScoreAfterIncrease){
+                                vehicleCount -= 1;
+                                percentage = vehicleCount/numOfVehicles;
+                                changeInTrip = -1;
+                                if((percentage)*100 <= 75){
+                                    //System.out.println("count went below 75% "+ vehicleCount+"/"+numOfVehicles);
+                                    break;
+                                }
+                            }else{
+                                vehicleCount += 1;
+                                percentage = vehicleCount/numOfVehicles;
+                                changeInTrip = 1;
+                                if((percentage)*100 >= 125) {
+                                    //System.out.println("count went above 125% " + vehicleCount + "/" + numOfVehicles);
+                                    break;
+                                }
+                            }
+
                             Set<Id<Link>> countStations1 = actualCount.keySet();
                             for (Id<Link> link : countStations1) {
-                                percentage = modifiedActualCount.get(link)/actualCountForReference.get(link);
-                                if (linkIds.contains(link)) {
-                                    if((percentage)*100 <= 75){
-                                        System.out.println("count went below 75% "+ modifiedActualCount.get(link)+"/"+actualCountForReference.get(link));
-                                        break;
-                                    }
-
-                                    double decreaseTrip = modifiedActualCount.get(link) - 1;
+                                if (linkIdsInRoute.contains(link)) {
+                                    double decreaseTrip = modifiedActualCount.get(link) + changeInTrip;
                                     modifiedActualCount.put(link, decreaseTrip);
                                 }
                             }
-                            if((percentage)*100 <= 75)
-                                break;
+
                             //score --- check optimized score is less than the optimized score of previous step
                             double optimizedScore = calculateScore(modifiedActualCount, trafficCountReference);
                             if((optimizedPreviousScore < optimizedScore) && optimizedPreviousScore != 0)
@@ -90,63 +106,27 @@ public class Optimization {
                                 optimizedPreviousScore = optimizedScore;
 
                             if (optimizedScore < score){
-                                System.out.println("Optimized Score is "+optimizedScore+" and initial score is "+score);
+                                //System.out.println("Optimized Score is "+optimizedScore+" and initial score is "+score);
                                 Set<Id<Link>> countStations = actualCount.keySet();
+                                totalTripsChanged.addAndGet(Math.abs(changeInTrip));
                                 for(Id<Link> link : countStations){
                                     actualCount.put(link, modifiedActualCount.get(link));
                                 }
-                                increment += 1;
                             }else
                                 increment = 0;
                         }
-                    }else{
-                        Set<Id<Link>> actualCountIncreaseStations = actualCountIncrease.keySet();
-                        for(Id<Link> link : actualCountIncreaseStations){
-                            modifiedActualCount.put(link, actualCountIncrease.get(link));
-                        }
-                        while(increment > 0) {
-                            Set<Id<Link>> countStations2 = actualCount.keySet();
-                            for (Id<Link> link : countStations2) {
-                                percentage = actualCountForReference.get(link)/modifiedActualCount.get(link);
-                                if (linkIds.contains(link)) {
-                                    if((percentage)*100 >= 125){
-                                        System.out.println("count went above 125% "+actualCountForReference.get(link)+"/"+modifiedActualCount.get(link));
-                                        break;
-                                    }
-                                    double increaseTrip = modifiedActualCount.get(link) + 1;
-                                    modifiedActualCount.put(link, increaseTrip);
-                                }
-                            }
-                            if((percentage)*100 >= 125)
-                                break;
-                            //score
-                            double optimizedScore = calculateScore(modifiedActualCount, trafficCountReference);
-                            if((optimizedPreviousScore < optimizedScore) && optimizedPreviousScore != 0)
-                                break;
-                            else
-                                optimizedPreviousScore = optimizedScore;
-
-                            if (optimizedScore < score){
-                                System.out.println("Optimized score is "+optimizedScore+" and initial score is "+score);
-                                Set<Id<Link>> countStations = actualCount.keySet();
-                                for(Id<Link> link : countStations){
-                                    actualCount.put(link, modifiedActualCount.get(link));
-                                }
-                                increment += 1;
-                            } else
-                                increment = 0;
-                        }
-                    }
                 }
+            }
             });
         });
         System.out.println("Final optimized score is "+calculateScore(actualCount, trafficCountReference));
+        System.out.println("Total number of trips "+totalTrips);
+        System.out.println("Total number of trips changed "+totalTripsChanged);
         try {
             writeOptimizedActualCount(actualCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
     }
 
     private static double calculateScore(Map<Id<Link>, Double> actualCount, Map<Id<Link>, Double> trafficCountReference) {
